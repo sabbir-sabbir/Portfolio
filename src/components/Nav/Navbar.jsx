4254b5d81f3ff1e7330400f54aab869c59e18c83
sabbir-sabbir <iosabbir7890@gmail.com>
2025-04-21 16:42:46 +0600

    add

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className={`fixed z-50   w-screen h-auto py-2 px-3 sm:px-3 md:px-4 lg:px-3 xl:px-4 border-b-1 border-white   ${D ? 'bg-gradient-to-r from-zinc-800 via-zinc-800/80 to-zinc-800' : 'bg-gradient-to-r from-white via-[#caf0f8] to-white'} `}>
+        <div className={`fixed z-50   w-screen h-auto py-1 px-3  md:px-4 lg:px-3 xl:px-4 border-b-1 border-white   ${D ? 'bg-gradient-to-r from-black via-black to-black' : 'bg-gradient-to-r from-white via-[#e5e5e5] to-[#dee2e6]'} `}>
           <div className="flex justify-start items-center gap-24">
             <div className="flex items-center gap-1">
               {" "}
@@ -22,7 +22,7 @@ const Navbar = () => {
               whileDrag={{
                 scale: 1.9,
               }}
-                  className="w-[100px] h-[50px] animate-spin [animation-duration:5s] hover:scale-75 transition-all "
+                  className="w-[80px] h-[40px] animate-spin [animation-duration:3s] hover:scale-75 transition-all "
                   src="/navlogo.svg"
                   alt="logo"
                 />
@@ -31,13 +31,13 @@ const Navbar = () => {
             </div>
 
             {/* second div */}
-            <div className="flex items-center gap-8 md:mr-16">
+            <div className="flex items-center gap-8 ">
               <div>
                 {" "}
                 <Link
                  smooth={true} offset={-80} duration={500}
                   to='interface'
-                  className={`font-light leading-tight text-[18px] cursor-pointer ${D ? 'text-white' : 'text-black'}`}
+                  className={`font-audiowide text-[18px] cursor-pointer hover:text-[22px] transition-all duration-300 ${D ? 'text-white' : 'text-black'}`}
                 >
                   Interface{" "}
                 </Link>
@@ -46,7 +46,7 @@ const Navbar = () => {
                 <Link
                   to='skill'
                   smooth={true} offset={-60} duration={500}
-                  className={`font-light leading-tight text-[18px] cursor-pointer ${D ? 'text-white' : 'text-black'}`}
+                  className={`font-audiowide text-[18px] cursor-pointer  hover:text-[22px] transition-all duration-300 ${D ? 'text-white' : 'text-black'}`}
                 >
                   Skills
                 </Link>
@@ -55,7 +55,7 @@ const Navbar = () => {
                 <Link
                   smooth={true} offset={-80} duration={500}
                   to='works'
-                  className={`font-light leading-tight text-[18px] cursor-pointer ${D ? 'text-white' : 'text-black'}`}
+                  className={`font-audiowide text-[18px] cursor-pointer  hover:text-[22px] transition-all duration-300 ${D ? 'text-white' : 'text-black'}`}
 
                 >
                   Works
