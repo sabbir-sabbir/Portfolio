4c30c82f8e5e126154e7fe61d0a0214d811f1b68
sabbir-sabbir <iosabbir7890@gmail.com>
2025-05-05 13:14:18 +0600

    add

--- src/components/Works/Card.jsx
+++ src/components/Works/Card.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useContext } from "react";
 import { motion } from "framer-motion";
 import { Pro } from "../Context/Context";
+import cardData from "./cardData";
 
 const Card = () => {
-  const { D, setD } = useContext(Pro);
+  const { D } = useContext(Pro);
   const [card, setCard] = useState(false);
 
   const Variant1 = {
@@ -39,126 +40,56 @@ const Card = () => {
     },
   };
 
+  
   return (
-    <>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="flex flex-col items-center justify-center gap-1 py-2 px-2">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {cardData.map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center justify-center gap-1 py-2 px-2"
+        >
           <motion.div
             animate={card ? "hide" : "show"}
             variants={Variant1}
-            className={`flex flex-col justify-center items-center gap-2  py-2 px-2 ${
+            className={`flex flex-col justify-center items-center gap-2 py-2 px-2 ${
               D
                 ? "bg-gray-700"
                 : "bg-gradient-to-r from-[#e0aaff]/25 via-[#caf0f8]/35 to-[#e9ecef]"
-            } `}
+            }`}
           >
             <motion.div variants={Variant2}>
               <img className="w-18 h-18" src="/git.svg" alt="" />
             </motion.div>
             <motion.p>
               <a
-                className={` underline-offset-1 underline ${D ? 'text-zinc-300' : 'text-zinc-600'}`} 
-                href="https://github.com/sabbir-sabbir/Website-for-College"
+                className={`underline-offset-1 underline ${
+                  D ? "text-zinc-300" : "text-zinc-600"
+                }`}
+                href={item.href}
                 target="blank"
               >
                 See The Preview Link inside Repository --- <br />
-                This Project Made With React JS, Tailwind css, Web3 API, &
-                Vanila JS
+                {item.description}
               </a>
             </motion.p>
           </motion.div>
-          <motion.div drag
-              dragSnapToOrigin={true}
-              dragTransition={{bounceStiffness: 300, bounceDamping: 10}}
-              whileDrag={{
-                scale: 1.1,
-              }}
+
+          <motion.div
+            drag
+            dragSnapToOrigin={true}
+            dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
+            whileDrag={{ scale: 1.1 }}
             onClick={() => setCard(!card)}
             className="flex items-center justify-center cursor-pointer py-2 px-2 w-full bg-green-300 gap-3 hover:scale-105 duration-300"
           >
             <button className="cursor-pointer">
               {!card ? "Click to Close" : "Click to View"}
-            </button>{" "}
-            <img className="w-6 h-6 " src="/view.svg" alt="" />{" "}
+            </button>
+            <img className="w-6 h-6" src="/view.svg" alt="" />
           </motion.div>
         </div>
-
-        {/* second col */}
-        <div className="flex flex-col items-center justify-center gap-1 py-2 px-2">
-          <motion.div
-            animate={card ? "hide" : "show"}
-            variants={Variant1}
-            className={`flex flex-col justify-center items-center gap-2  py-2 px-2 ${D ? 'bg-gray-700' : 'bg-gradient-to-r from-[#e0aaff]/25 via-[#caf0f8]/35 to-[#e9ecef]'} `}
-          >
-            <motion.div variants={Variant2}>
-              <img className="w-18 h-18" src="/git.svg" alt="" />
-            </motion.div>
-            <motion.p>
-              <a
-               className={` underline-offset-1 underline ${D ? 'text-zinc-300' : 'text-zinc-600'}`}
-                href="https://github.com/sabbir-sabbir/Nike-Brand-Website"
-                target="blank"
-              >
-                See The Preview Link inside Repository --- <br />
-                This Project Made With React JS, Tailwind CSS,  Vanila
-                JS & More
-              </a>
-            </motion.p>
-          </motion.div>
-          <motion.div drag
-              dragSnapToOrigin={true}
-              dragTransition={{bounceStiffness: 300, bounceDamping: 10}}
-              whileDrag={{
-                scale: 1.1,
-              }}
-            onClick={() => setCard(!card)}
-            className="flex items-center justify-center cursor-pointer py-2 px-2 w-full bg-green-300 gap-3 hover:scale-105 duration-300 "
-          >
-            <button className="cursor-pointer">
-              {!card ? "Click to Close" : "Click to View"}
-            </button>{" "}
-            <img className="w-6 h-6 " src="/view.svg" alt="" />{" "}
-          </motion.div>
-        </div>
-
-        {/* Thirt col */}
-        <div className="flex flex-col items-center justify-center gap-1 py-2 px-2">
-          <motion.div
-            animate={card ? "hide" : "show"}
-            variants={Variant1}
-            className={`flex flex-col justify-center items-center gap-2  py-2 px-2 ${D ? 'bg-gray-700' : 'bg-gradient-to-r from-[#e0aaff]/25 via-[#caf0f8]/35 to-[#e9ecef]'} `}
-          >
-            <motion.div variants={Variant2}>
-              <img className="w-18 h-18" src="/git.svg" alt="" />
-            </motion.div>
-            <motion.p>
-              <a
-              className={` underline-offset-1 underline ${D ? 'text-zinc-300' : 'text-zinc-600'}`}
-                href="https://github.com/sabbir-sabbir/my-app"
-                target="blank"
-              >
-                See The Preview Link inside Repository --- <br />
-                This Project Made With Next JS, Tailwind css, TypeScript, ShadCN UI, & Tanstack Table.
-              </a>
-            </motion.p>
-          </motion.div>
-          <motion.div drag
-              dragSnapToOrigin={true}
-              dragTransition={{bounceStiffness: 300, bounceDamping: 10}}
-              whileDrag={{
-                scale: 1.1,
-              }}
-            onClick={() => setCard(!card)}
-            className="flex items-center justify-center cursor-pointer py-2 px-2 w-full bg-green-300 gap-3 hover:scale-105 duration-300"
-          >
-            <button className="cursor-pointer">
-              {!card ? "Click to Close" : "Click to View"}
-            </button>{" "}
-            <img className="w-6 h-6 " src="/view.svg" alt="" />{" "}
-          </motion.div>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
---
+++ src/components/Works/cardData.js
@@ -0,0 +1,19 @@
+const cardData = [
+    {
+      href: "https://github.com/sabbir-sabbir/Website-for-College",
+      description:
+        "This Project Made With React JS, Tailwind css, Web3 API, & Vanila JS",
+    },
+    {
+      href: "https://github.com/sabbir-sabbir/Nike-Brand-Website",
+      description:
+        "This Project Made With React JS, Tailwind CSS,  Vanila JS & More",
+    },
+    {
+      href: "https://github.com/sabbir-sabbir/my-app",
+      description:
+        "This Project Made With Next JS, Tailwind css, TypeScript, ShadCN UI, & Tanstack Table.",
+    },
+  ];
+
+  export default cardData;
